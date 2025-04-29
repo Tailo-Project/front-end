a4b06ccc756e23bedeb03a41bb57fa2dd58d6c9e
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2025-04-29 20:15:59 +0900

    refactor: DMInbox 컴포넌트에서 React import 수정

--- src/components/features/dm/DMInbox.tsx
+++ src/components/features/dm/DMInbox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { BASE_API_URL } from '@/constants/apiUrl';
 import { fetchWithToken } from '@/token';
